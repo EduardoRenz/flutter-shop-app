5c08bbee5ffb61898fcbf0f5fa6b9e73c03bb378
Eduardo <dudarenz@gmail.com>
2022-01-30 01:00:32 -0300

    componentized product grid

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
+import 'package:shop/components/product_grid.dart';
 import 'package:shop/models/product.dart';
 import 'package:shop/models/product_list.dart';
 
@@ -15,16 +15,6 @@ class ProductsOverviewScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('MyShop'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: GridView.builder(
-              itemCount: loadedProducts.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 3 / 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10),
-              itemBuilder: (ctx, i) => ProductItem(loadedProducts[i])),
-        ));
+        body: const ProductGrid());
   }
 }
