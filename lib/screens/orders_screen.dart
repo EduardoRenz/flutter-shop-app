f4c2f4c9ec84ee87d98ace48843e6d330ec26646
Eduardo <dudarenz@gmail.com>
2022-03-02 01:08:47 -0300

    using future builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,41 +4,31 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _isLoading = true;
-    Provider.of<OrderList>(context, listen: false)
-        .loadOrders()
-        .then((_) => setState(() => _isLoading = false));
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderList = Provider.of<OrderList>(context);
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Orders'),
-      ),
-      drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orderList.orders.length,
-              itemBuilder: (_, index) =>
-                  OrderWidget(order: orderList.orders[index]),
-            ),
-    );
+        appBar: AppBar(
+          title: const Text('My Orders'),
+        ),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return Consumer<OrderList>(
+                builder: (ctx, orders, child) => ListView.builder(
+                  itemCount: orders.orders.length,
+                  itemBuilder: (_, index) =>
+                      OrderWidget(order: orders.orders[index]),
+                ),
+              );
+            }
+          },
+        ));
   }
 }
