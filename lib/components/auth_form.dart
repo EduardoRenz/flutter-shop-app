bc6b49940b5b66661408674ca52481fc726b1168
Eduardo <dudarenz@gmail.com>
2022-03-19 21:40:18 -0300

    refactor child form on Animated Builder to better performance

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -109,87 +109,86 @@ class _AuthFormState extends State<AuthForm>
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return AnimatedBuilder(
-        animation: _heightAnimation!,
-        builder: (ctx, child) => SizedBox(
-              height: _heightAnimation!.value.height,
-              width: deviceSize.width * 0.75,
-              child: Card(
-                elevation: 8,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        onSaved: (email) => _authData['email'] = email ?? '',
-                        validator: (_email) {
-                          if (_email == null) {
-                            return 'Please enter a valid email';
-                          }
-                          // regex to match valid email
-                          final regex = RegExp(
-                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-                          if (!regex.hasMatch(_email)) {
-                            return 'Please enter a valid email';
+      animation: _heightAnimation!,
+      builder: (ctx, childForm) => SizedBox(
+          height: _heightAnimation!.value.height,
+          width: deviceSize.width * 0.75,
+          child: childForm),
+      child: Card(
+        elevation: 8,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (email) => _authData['email'] = email ?? '',
+                validator: (_email) {
+                  if (_email == null) {
+                    return 'Please enter a valid email';
+                  }
+                  // regex to match valid email
+                  final regex = RegExp(
+                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+                  if (!regex.hasMatch(_email)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+                onSaved: (password) => _authData['password'] = password ?? '',
+                controller: _passwordController,
+                validator: (_password) => (_password != null &&
+                        (_password.isEmpty || _password.length < 6))
+                    ? 'Password must be at least 6 characters'
+                    : null,
+              ),
+              if (_isSignup())
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Confirm Password',
+                  ),
+                  obscureText: true,
+                  validator: _isLogin()
+                      ? null
+                      : (_password) {
+                          if (_password != _passwordController.text) {
+                            return 'Passwords do not match';
                           }
                           return null;
                         },
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Password',
-                        ),
-                        obscureText: true,
-                        onSaved: (password) =>
-                            _authData['password'] = password ?? '',
-                        controller: _passwordController,
-                        validator: (_password) => (_password != null &&
-                                (_password.isEmpty || _password.length < 6))
-                            ? 'Password must be at least 6 characters'
-                            : null,
-                      ),
-                      if (_isSignup())
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Confirm Password',
-                          ),
-                          obscureText: true,
-                          validator: _isLogin()
-                              ? null
-                              : (_password) {
-                                  if (_password != _passwordController.text) {
-                                    return 'Passwords do not match';
-                                  }
-                                  return null;
-                                },
-                        ),
-                      const SizedBox(height: 20),
-                      if (_isLoading)
-                        const CircularProgressIndicator()
-                      else
-                        SizedBox(
-                          width: deviceSize.width * 0.7,
-                          child: ElevatedButton(
-                            onPressed: _submit,
-                            child: Text(_authMode.name.toUpperCase()),
-                          ),
-                        ),
-                      const Spacer(),
-                      TextButton(
-                          onPressed: () {
-                            _switchAuthMode();
-                          },
-                          child: Text(_isLogin() ? 'Signup' : 'Login')),
-                    ]),
+                ),
+              const SizedBox(height: 20),
+              if (_isLoading)
+                const CircularProgressIndicator()
+              else
+                SizedBox(
+                  width: deviceSize.width * 0.7,
+                  child: ElevatedButton(
+                    onPressed: _submit,
+                    child: Text(_authMode.name.toUpperCase()),
                   ),
                 ),
-              ),
-            ));
+              const Spacer(),
+              TextButton(
+                  onPressed: () {
+                    _switchAuthMode();
+                  },
+                  child: Text(_isLogin() ? 'Signup' : 'Login')),
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
